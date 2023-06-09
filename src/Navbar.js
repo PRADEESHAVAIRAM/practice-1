3c07dc42fd4d2214f5df5b6f4c997244470baffb
PRADEESHAVAIRAM <pradeesha.vairam@kprinfo.com>
2023-06-09 16:21:31 +0530

    new changes

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+ const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>THE NEW HOME PAGE</h1>
+            <div className="links">
+                <a href="/">HOME</a>
+                <a href="/create">New WEBSITE</a>
+            </div>
+        </nav>
+     );
+ }
+  
+ export default Navbar;
